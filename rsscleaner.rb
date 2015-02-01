c67004828222366f10a0e3984bffef561ecbb95f
repat <repat@repat.de>
2015-02-01 06:25:31 +0100

    ohrenbaer url

--- rsscleaner.rb
+++ rsscleaner.rb
@@ -4,8 +4,7 @@ require 'rss/2.0'
 require 'open-uri'
 
 urls = {}
-urls["localnews"] = "http://www.codyenterprise.com/search/?q=&t=article&l=100&d=&d1=&d2=&s=start_time&sd=desc&c[]=news/local,news/local/*&f=rss"
-urls["codyobits"] = "http://www.codyenterprise.com/search/?q=&t=article&l=100&d=&d1=&d2=&s=start_time&sd=desc&c[]=news/obituaries,news/obituaries/*&f=rss"
+urls["ohrenbaer"] = "http://www.ohrenbaer.de/podcast/podcast.feed.podcast.xml"
 
 get '/:slug' do
   source = urls[params[:slug].to_s]
